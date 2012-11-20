a0b8387a6200528ccd040b87c73b02c54caa3449
Ville Tuulos <ville@bitdeli.com>
2012-11-19 19:23:53 -0800

    add docstrings to __init__.py

--- bitdeli/__init__.py
+++ bitdeli/__init__.py
@@ -1,3 +1,19 @@
+"""
+:mod:`bitdeli`: Interface to profiles
+=====================================
+
+This module is used by the :ref:`card-script` to access :ref:`profiles`.
+
+The :mod:`bitdeli` module supports a straightforward imperative coding style aka :ref:`the classic flavor <python-flavors>`:
+
+.. code-block:: python
+
+    import bitdeli
+    from bitdeli.widgets import Text
+
+    for profile in bitdeli.profiles():
+        Text(head='UID %s' % profile.uid)
+"""
 import os
 from datetime import datetime, timedelta
 from types import DictType
@@ -18,6 +34,13 @@ if 'TESTING' not in os.environ:
 GROUP_FORMAT = '%Y-%m-%d'
 
 class Profile(dict):
+    """
+    The :class:`Profile` object is inherited from `the standard Python dictionary <http://docs.python.org/2/library/stdtypes.html#mapping-types-dict>`_. You can use any dictionary methods to access the fields in this :ref:`profile <profiles>`.
+
+    Modifications to the dictionary are not persisted. Use :func:`profiles` to instantiate the object.
+
+    The field **uid** contains the unique identifier of this profile.
+    """
     def __init__(self, entry):
         uid, data = entry
         self.uid = uid
@@ -30,6 +53,20 @@ class Profile(dict):
 
     def close(self):
         output([(self.uid, self)])
+
+
+def profiles():
+    """
+    Returns an iterator that iterates over :ref:`profiles`.
+
+    Each profile is a :class:`Profile` object.
+    """
+    for did, entry in entries():
+        yield Profile(entry)
+
+#
+# used by profile scripts
+#
 
 def profile_events():
     decoder = Decoder(json_decode=BenJson, lazylist_obj=ChunkedList)
@@ -47,9 +84,5 @@ def profile_events():
         for event in events:
             pass
         profile.close()
-
-def profiles():
-    for did, entry in entries():
-        yield Profile(entry)
 
 encoder_alias(Profile, DictType)
