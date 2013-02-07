01832aabcdf5f9d4914c39f632c4307fcc506310
Ville Tuulos <ville@bitdeli.com>
2013-02-07 22:58:12 +0000

    create & load discodb in __main__ / model

--- __main__.py
+++ __main__.py
@@ -3,24 +3,35 @@ import json
 from bitdeli import model
 from bitdeli import insight
 
-from bitdeli.protocol import params, output
-from bitdeli.bencode import BenJson
+from bitdeli.protocol import params, output, entries
+from bitdeli.bencode import BenJson, bencode
 from bitdeli import profiles
+
+DDB_PART_SIZE = 8 * 1024 * 1024
 
 PARAMS = params()
 
 def do_insight():
+    def load_model():
+        from discodb import DiscoDB
+        return DiscoDB.loads(''.join(part for did, part in entries()))
     import model as _
     import insight as _
-    # FIXME load model data (input)
-    m = model._load(None) # FIXME add segments
+    m = model._load(load_model(), None) # FIXME add segments
+    # FIXME handle missing _run
     widgets = insight._run(m, PARAMS['event'])
     output(map(lambda x: BenJson(json.dumps(x)), widgets))
 
 def do_model():
+    def split(data):
+        i = 0
+        while i < len(data):
+            yield data[i:i + DDB_PART_SIZE]
+            i += DDB_PART_SIZE
     import model as _
-    model._new(profiles())
-    # FIXME save model here?
+    # FIXME handle missing _ddb
+    ddb = model._ddb(profiles())
+    output(split(ddb.dumps()))
 
 if __name__ == '__main__':
     mtype = PARAMS['type']
--- bitdeli/model.py
+++ bitdeli/model.py
@@ -1,16 +1,19 @@
-
-_new = None
+_ddb = None
 
 def new(func):
-    global _new
-    _new = func
-    # FIXME func could be a generator of k-v pairs:
-    # create discodb in a wrapper func
+    from discodb import DiscoDB
+    global _ddb
+    _ddb = lambda profiles: DiscoDB(func(profiles))
     return func
 
-def _load(segments):
+def new_discodb(func):
+    global _ddb
+    _ddb = func
+    return func
+
+def _load(model, segments):
     if segments:
         # FIXME return segmented model
         pass
     else:
-        return 'this is a model'
+        return model
