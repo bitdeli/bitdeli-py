41ec0d26b903972d677e39a2f35b8bd1a66716e7
Ville Tuulos <ville@bitdeli.com>
2013-02-11 23:48:44 +0000

    support drafts in __main__.py

--- __main__.py
+++ __main__.py
@@ -11,33 +11,36 @@ DDB_PART_SIZE = 8 * 1024 * 1024
 
 PARAMS = params()
 
-def do_insight():
-    def load_model():
-        from discodb import DiscoDB
-        return DiscoDB.loads(''.join(part for did, part in entries()))
+def load_model():
+    from discodb import DiscoDB
+    return DiscoDB.loads(''.join(part for did, part in entries()))
+
+def split(data):
+    i = 0
+    while i < len(data):
+        yield data[i:i + DDB_PART_SIZE]
+        i += DDB_PART_SIZE
+
+def do_insight(db):
     import model as _
     import insight as _
-    m = model._load(load_model(), None) # FIXME add segments
+    m = model._load(db, None) # FIXME add segments
     # FIXME handle missing _run
     widgets = insight._run(m, PARAMS['event'])
-    output(map(lambda x: BenJson(json.dumps(x)), widgets))
+    return map(lambda x: BenJson(json.dumps(x)), widgets)
 
 def do_model():
-    def split(data):
-        i = 0
-        while i < len(data):
-            yield data[i:i + DDB_PART_SIZE]
-            i += DDB_PART_SIZE
     import model as _
     # FIXME handle missing _ddb
-    ddb = model._ddb(profiles())
-    output(split(ddb.dumps()))
+    return model._ddb(profiles())
 
 if __name__ == '__main__':
     mtype = PARAMS['type']
     if mtype == 'insight':
-        do_insight()
+        output(do_insight(load_model()))
     elif mtype == 'model':
-        do_model()
+        output(split(do_model().dumps()))
+    elif mtype == 'draft':
+        output(do_insight(do_model()))
     else:
         raise Exception('Unknown type: %s' % mtype)
